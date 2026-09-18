2b0567c08789d2ee104337c5c2b5c200ad83774c
manoj19-03 <manoj.h@wipro.com>
2026-09-18 15:22:32 +0530

    new update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GitHub Actions CI/CD Demo! version-3"
+    return "Hello from GitHub Actions CI/CD Demo!"
 
 @app.route("/health")
 def health():
